ccc59ba0340c5b101e8c6c04cf2968b60978b182
Álvaro Saúl Rodríguez Alemán <>
2018-11-08 09:39:05 +0000

    v2.0

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,8 +1,11 @@
 package kata5p1;
 
+import static kata5p1.CrearTabla.createNewTable;
+
 public class Kata5P1 {
     public static void main(String[] args) {
         SelectApp app = new SelectApp();
         app.selectAll();
+        createNewTable("EMAIL");
     }
 }
